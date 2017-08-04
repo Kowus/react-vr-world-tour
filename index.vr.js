9da2e8684c00b9bd02228e4578491ff9b9b2b40d
kowus <barnabas@elite-education.org>
2017-08-04 03:56:55 +0000

    init project

---
+++ index.vr.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {View, Text, Pano, AppRegistry, asset} from 'react-vr';
+
+class WorldTour extends Component{
+    render(){
+        return(
+            <View>
+                <Pano source={asset('starry-sky.jpg')}></Pano>
+            </View>
+        );
+    }
+};
+AppRegistry.registerComponent('WorldTour', () => WorldTour);
